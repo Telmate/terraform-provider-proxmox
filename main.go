0894e06226bae45f0fd2a61e096cd7504976d713
Phil Porada <pporada@letsencrypt.org>
2021-04-29 16:12:06 -0400

    Ran gofmt

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"github.com/Telmate/terraform-provider-proxmox/proxmox"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func main() {
