6a82251c4d089d935851a1c5c2b0a9c45e8130fe
SourceFellows <info@source-fellows.com>
2021-05-07 05:34:08 +0200

    Added the ability to debug the plugin through the Terraform Plugin SDK. See also: https://www.terraform.io/docs/extend/debugging.html#enabling-debugging-in-a-provider

--- cmd/terraform-provider-proxmox/main.go
+++ cmd/terraform-provider-proxmox/main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"github.com/Telmate/terraform-provider-proxmox/proxmox"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"log"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
+
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
 			return proxmox.Provider()
 		},
-	})
+	}
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "registry.terraform.io/telmate/proxmox", opts)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		return
+	}
+
+	plugin.Serve(opts)
 }
