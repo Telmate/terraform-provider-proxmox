0037daaa1def76d977060bcbe49b03996043fe30
mleone87 <maleone@thefork.com>
2022-10-09 18:49:00 +0200

    make regex more inclusive

--- proxmox/resource_vm_qemu.go
+++ proxmox/resource_vm_qemu.go
@@ -1590,7 +1590,7 @@ func _resourceVmQemuRead(d *schema.ResourceData, meta interface{}) error {
 
 	// need to set cache because proxmox-api-go requires a value for cache but doesn't return a value for
 	// it when it is empty. thus if cache is "" then we should insert "none" instead for consistency
-	var rxCloudInitDrive = regexp.MustCompile(`^.*-cloudinit$`)
+	var rxCloudInitDrive = regexp.MustCompile(`^.*-cloudinit.*`)
 	for id, qemuDisk := range config.QemuDisks {
 		logger.Debug().Int("vmid", vmID).Msgf("[READ] Disk Processed '%v'", qemuDisk)
 		// ugly hack to avoid cloudinit disk to be removed since they usually are not present in resource definition
