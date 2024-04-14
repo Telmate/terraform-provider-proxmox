b1cfcbeca22d0869cccec7fd2ce98f3bbf02e31e
Tinyblargon <76069640+Tinyblargon@users.noreply.github.com>
2024-04-14 20:23:52 +0200

    fix: `storage` should be optional

--- proxmox/resource_vm_qemu.go
+++ proxmox/resource_vm_qemu.go
@@ -501,7 +501,7 @@ func resourceVmQemu() *schema.Resource {
 								return diag.Errorf("slot must be one of 'ide0', 'ide1', 'ide2', 'sata0', 'sata1', 'sata2', 'sata3', 'sata4', 'sata5', 'scsi0', 'scsi1', 'scsi2', 'scsi3', 'scsi4', 'scsi5', 'scsi6', 'scsi7', 'scsi8', 'scsi9', 'scsi10', 'scsi11', 'scsi12', 'scsi13', 'scsi14', 'scsi15', 'scsi16', 'scsi17', 'scsi18', 'scsi19', 'scsi20', 'scsi21', 'scsi22', 'scsi23', 'scsi24', 'scsi25', 'scsi26', 'scsi27', 'scsi28', 'scsi29', 'scsi30', 'virtio0', 'virtio1', 'virtio2', 'virtio3', 'virtio4', 'virtio5', 'virtio6', 'virtio7', 'virtio8', 'virtio9', 'virtio10', 'virtio11', 'virtio12', 'virtio13', 'virtio14', 'virtio15'")
 							},
 						},
-						"storage": schema_DiskStorage(),
+						"storage": schema_DiskStorage(schema.Schema{Optional: true}),
 						"type": {
 							Type:     schema.TypeString,
 							Optional: true,
@@ -3755,7 +3755,7 @@ func schema_Ide(setting string) *schema.Schema {
 							"replicate":      {Type: schema.TypeBool, Optional: true},
 							"serial":         schema_DiskSerial(),
 							"size":           schema_DiskSize(schema.Schema{Required: true}),
-							"storage":        schema_DiskStorage(),
+							"storage":        schema_DiskStorage(schema.Schema{Required: true}),
 							"wwn":            schema_DiskWWN(),
 						}),
 					},
@@ -3812,7 +3812,7 @@ func schema_Sata(setting string) *schema.Schema {
 							"replicate":      {Type: schema.TypeBool, Optional: true},
 							"serial":         schema_DiskSerial(),
 							"size":           schema_DiskSize(schema.Schema{Required: true}),
-							"storage":        schema_DiskStorage(),
+							"storage":        schema_DiskStorage(schema.Schema{Required: true}),
 							"wwn":            schema_DiskWWN(),
 						}),
 					},
@@ -3871,7 +3871,7 @@ func schema_Scsi(setting string) *schema.Schema {
 							"replicate":      {Type: schema.TypeBool, Optional: true},
 							"serial":         schema_DiskSerial(),
 							"size":           schema_DiskSize(schema.Schema{Required: true}),
-							"storage":        schema_DiskStorage(),
+							"storage":        schema_DiskStorage(schema.Schema{Required: true}),
 							"wwn":            schema_DiskWWN(),
 						}),
 					},
@@ -3931,7 +3931,7 @@ func schema_Virtio(setting string) *schema.Schema {
 							"replicate":      {Type: schema.TypeBool, Optional: true},
 							"serial":         schema_DiskSerial(),
 							"size":           schema_DiskSize(schema.Schema{Required: true}),
-							"storage":        schema_DiskStorage(),
+							"storage":        schema_DiskStorage(schema.Schema{Required: true}),
 							"wwn":            schema_DiskWWN(),
 						}),
 					},
@@ -4160,11 +4160,9 @@ func schema_DiskSize(s schema.Schema) *schema.Schema {
 	return &s
 }
 
-func schema_DiskStorage() *schema.Schema {
-	return &schema.Schema{
-		Type:     schema.TypeString,
-		Required: true,
-	}
+func schema_DiskStorage(s schema.Schema) *schema.Schema {
+	s.Type = schema.TypeString
+	return &s
 }
 
 func schema_DiskWWN() *schema.Schema {
