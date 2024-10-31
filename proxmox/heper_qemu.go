7d901b63b0c0cb5926fb5809867d24425c885fdd
Tinyblargon <76069640+Tinyblargon@users.noreply.github.com>
2024-10-31 22:17:55 +0100

    fix: wrong variable name in warning

--- proxmox/heper_qemu.go
+++ proxmox/heper_qemu.go
@@ -68,7 +68,7 @@ func (conn connectionInfo) agentDiagnostics() diag.Diagnostics {
 		return diag.Diagnostics{diag.Diagnostic{
 			Severity: diag.Warning,
 			Summary:  errorGuestAgentNoIPv6Summary,
-			Detail:   "Qemu Guest Agent is enabled in your configuration but no IPv6 address was found before the time ran out, increasing '" + schemaAgentTimeout + "' could resolve this issue. To suppress this warning set '" + schemaSkipIPv4 + "' to true."}}
+			Detail:   "Qemu Guest Agent is enabled in your configuration but no IPv6 address was found before the time ran out, increasing '" + schemaAgentTimeout + "' could resolve this issue. To suppress this warning set '" + schemaSkipIPv6 + "' to true."}}
 	}
 	return diag.Diagnostics{}
 }
