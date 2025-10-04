69c422b009d5c9324570c05a3db7abc424243f24
Nemo D'ACREMONT <github@dacremont.xyz>
2025-10-04 14:58:44 +0200

    feat: add storage template to enable downloading lxc templates from terraform

---
+++ proxmox/resource_storage_template.go
@@ -0,0 +1,122 @@
+package proxmox
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/Telmate/proxmox-api-go/proxmox"
+	pveSDK "github.com/Telmate/proxmox-api-go/proxmox"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+const (
+	templateContentType = "vztmpl"
+)
+
+func resourceStorageTemplate() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceStorageTemplateCreate,
+		ReadContext:   resourceStorageTemplateRead,
+		DeleteContext: resourceStorageTemplateDelete,
+
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"pve_node": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"storage": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"template": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+		},
+		Timeouts: resourceTimeouts(),
+	}
+}
+
+func resourceStorageTemplateCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	pconf := meta.(*providerConfiguration)
+	client := pconf.Client
+	lock := pmParallelBegin(pconf)
+	defer lock.unlock()
+
+	template := d.Get("template").(string)
+	storage := d.Get("storage").(string)
+	node := d.Get("pve_node").(string)
+
+	config := proxmox.ConfigContent_Template{
+		Node:     node,
+		Storage:  storage,
+		Template: template,
+	}
+	if err := config.Validate(); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err := proxmox.DownloadLxcTemplate(ctx, client, config); err != nil {
+		return diag.FromErr(err)
+	}
+
+	volId := fmt.Sprintf("%s:%s/%s", storage, templateContentType, template)
+	d.SetId(volId)
+
+	return resourceStorageTemplateRead(ctx, d, meta)
+}
+
+func resourceStorageTemplateRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	pconf := meta.(*providerConfiguration)
+	client := pconf.Client
+
+	var templateFound bool
+	storage := d.Get("storage").(string)
+	nodeName := pveSDK.NodeName(d.Get("pve_node").(string))
+
+	storageContent, err := client.GetStorageContent(ctx, storage, nodeName)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	contents := storageContent["data"].([]interface{})
+	for c := range contents {
+		contentMap := contents[c].(map[string]interface{})
+		if contentMap["volid"].(string) == d.Id() {
+			size := contentMap["size"].(float64)
+			d.Set("size", ByteCountIEC(int64(size)))
+			templateFound = true
+			break
+		}
+	}
+
+	if !templateFound {
+		d.SetId("")
+	}
+
+	return nil
+}
+
+func resourceStorageTemplateDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	pconf := meta.(*providerConfiguration)
+	client := pconf.Client
+	lock := pmParallelBegin(pconf)
+	defer lock.unlock()
+
+	storage := strings.SplitN(d.Id(), ":", 2)[0]
+	templateURL := fmt.Sprintf("/nodes/%s/storage/%s/content/%s", d.Get("pve_node").(string), storage, d.Id())
+
+	if err := client.Delete(ctx, templateURL); err != nil {
+		return diag.FromErr(err)
+	}
+	return nil
+}
